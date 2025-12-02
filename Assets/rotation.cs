17bc3cbfe84420c9c826763e01e48ce3ae5c6c7b
Names <40932@ma-web.nl>
2025-12-02 14:48:43 +0100

    3de commit

--- Assets/rotation.cs
+++ Assets/rotation.cs
@@ -3,15 +3,23 @@ using UnityEngine.InputSystem.Controls;
 
 public class rotation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float amplitude = 1.0f; // hoe hoog/laag de sins golf
+    public float frequency = 1.0f; // hoe snell
+    public float rotateSpeed = 90.0f;
+
+    private Vector3 startPosition;
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 90f * Time.deltaTime, 0);
+        float newY = startPosition.y * amplitude * Mathf.Sin(Time.time * frequency);
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
     }
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # git_unity
 ## ![gif](gif/wafasdfaf.gif)
 ### [code](Assets/rotation.cs)
+
+## ![gif](gif/wafasdfaf312313.gif)
+### [code](Assets/rotation.cs)
